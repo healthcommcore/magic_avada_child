4d342d4d4a6e6e1f13b514078e32fee2bccd75ff
David Rothfarb <david@dr809.dfci.harvard.edu>
2020-01-10 15:04:38 -0500

    Initial commit with good portion of styles in place

---
+++ README.md
@@ -0,0 +1,2 @@
+# MaGIC Avada Child
+This is a child theme that uses the Avada parent theme as a foundation. There are several template overrides and I used a Gulp build environment along with SASS for styles/formatting.
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const dir = {
+  src : 'sass/style.scss',
+  build: './'
+};
+
+const 
+  gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  postcss = require('gulp-postcss'),
+  cssnano = require('cssnano'),
+  autoprefixer = require('autoprefixer'),
+  browsersync = require('browser-sync').create()
+;
+
+const clean = () => {
+  console.log(dir.src);
+  return gulp
+  .src(dir.src)
+  .pipe(sourcemaps.init())
+  .pipe(sass()).on('error', sass.logError)
+  .pipe(postcss([autoprefixer(), cssnano()]))
+  .pipe(sourcemaps.write('maps/'))
+  .pipe(gulp.dest(dir.build))
+  .pipe(browsersync.stream());
+};
+
+const reload = () => {
+  browsersync.reload();
+}
+
+const watch = () => {
+  browsersync.init({
+    proxy: "magic.dr809.test"
+  });
+  gulp.watch(dir.src, clean).on('change', browsersync.reload);
+}
+
+const build = gulp.parallel(clean, watch);
+
+exports.clean = clean;
+exports.watch = watch;
+exports.default = build;
